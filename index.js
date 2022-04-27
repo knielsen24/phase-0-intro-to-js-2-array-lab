77cc842bac6750bd77d5b08930ef4ed921917076
Kevin Nielsen <knielsen24@gmail.com>
2022-04-27 09:19:53 -0700

    commit tree b090e0d8d775a9d288500dce3958667ccb2aede8

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+const destructivelyAppendCat = name => cats.push(name);
+
+//console.log(destructivelyAppendCat("Ralph"));
+
+const destructivelyPrependCat = name => cats.unshift(name);
+
+//console.log(destructivelyPrependCat("Bob"))
+
+const destructivelyRemoveLastCat = name => cats.pop(name);
+
+//console.log(destructivelyAppendCat());
+
+const destructivelyRemoveFirstCat = name => cats.shift(name);
+
+const appendCat = name => [...cats, name];
+
+const prependCat = name => [name, ...cats];
+
+const removeLastCat = () => [...cats.slice(0, cats.length - 1)];
+
+const removeFirstCat = () => [...cats.slice(1)];
